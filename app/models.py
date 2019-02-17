9d322a33bb98f037c7a9f744a631c8fd6dc5a5fd
flaviocq <flavioq@uci.edu>
2019-02-16 18:05:16 -0800

    installed pymusixmatch

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     unique_word_count = db.Column(db.Integer())
+    narcissism_rating = db.Column(db.DECIMAL())
+    genre = db.Column(db.String(64), index=True, unique=False)
 
     def __repr__(self):
-        return '<Artist {}'.format(self.name)
+        return '<Artist {}>'.format(self.name)
